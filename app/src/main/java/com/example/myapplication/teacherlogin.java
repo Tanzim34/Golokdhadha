f6c44414b85b4f0b8795556a2f35c744d4592bd4
thebbyo <dibbyoroy7@gmail.com>
2023-08-17 10:04:34 +0600

    teacher signin

--- app/src/main/java/com/example/myapplication/teacherlogin.java
+++ app/src/main/java/com/example/myapplication/teacherlogin.java
@@ -2,13 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class teacherlogin extends AppCompatActivity {
 
+    EditText userName , passWord;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacherlogin);
+        Button button = findViewById(R.id.tlogin);
+        userName = findViewById(R.id.tUsername);
+        passWord = findViewById(R.id.tPassword);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sSignIn();
+            }
+        });
     }
+    void sSignIn(){
+        String user = userName.getText().toString();
+        String pass = passWord.getText().toString();
+        if(user.equals("admin") && pass.equals("admin")){
+            Toast.makeText(teacherlogin.this,"Successfull", Toast.LENGTH_SHORT).show();
+        }
+        else Toast.makeText(teacherlogin.this,"Invalid", Toast.LENGTH_SHORT).show();
+    }
+
 }
