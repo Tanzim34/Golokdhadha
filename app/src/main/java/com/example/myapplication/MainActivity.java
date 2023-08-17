6520c334c0fa4ab81a6f32851289214bc7873d2d
Tanzim34 <tanzimshafin411@gmail.com>
2023-08-17 08:48:53 +0600

    Initial

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boolean student = false;
+        Button stdbutton = findViewById(R.id.studentbutton);
+        Button teachbutton = findViewById(R.id.teacherbutton);
+        stdbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                setContentView(R.layout.signlogin);
+                Button back = findViewById(R.id.back);
+                Button login = findViewById(R.id.login);
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        gostdLogin();
+                    }
+                });
+                back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+        teachbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.signlogin);
+                Button login = findViewById(R.id.login);
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        goteachlogin();
+                    }
+                });
+            }
+        });
+    }
+    public void gostdLogin(){
+        Intent intent = new Intent(this, studentlogin.class);
+        startActivity(intent);
+    }
+    public void goteachlogin(){
+        Intent intent = new Intent(this, teacherlogin.class);
+        startActivity(intent);
+    }
+
+}
