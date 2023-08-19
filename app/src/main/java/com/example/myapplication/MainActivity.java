5d9735b2149909a0bf5f11aef6f3b049e4518d8e
thebbyo <dibbyoroy7@gmail.com>
2023-08-19 21:12:26 +0600

    TeacherLoginBackButton

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -64,6 +64,15 @@ public class MainActivity extends AppCompatActivity {
                         goTecSignup();
                     }
                 });
+                Button back = findViewById(R.id.back);
+                back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                });
             }
 
         });
