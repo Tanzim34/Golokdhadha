4d3ccdd707c913385a296c469c713c539cba98af
thebbyo <dibbyoroy7@gmail.com>
2023-08-20 13:28:58 +0600

    sign out

--- app/src/main/java/com/example/myapplication/afterlogin.java
+++ app/src/main/java/com/example/myapplication/afterlogin.java
@@ -2,7 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ktx.Firebase;
 
 public class afterlogin extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class afterlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afterlogin);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        Button signOutBtn = findViewById(R.id.signOut);
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(afterlogin.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
