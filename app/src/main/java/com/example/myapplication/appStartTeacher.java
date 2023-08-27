98b579379fecce3c9268e76a8b85111f3ed7340c
Tanzim34 <tanzimshafin411@gmail.com>
2023-08-28 02:11:12 +0600

    Added Layouts

--- app/src/main/java/com/example/myapplication/appStartTeacher.java
+++ app/src/main/java/com/example/myapplication/appStartTeacher.java
@@ -1,45 +1,39 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
 
-public class appstart extends AppCompatActivity {
-
+public class appStartTeacher extends AppCompatActivity {
 
     private FirebaseFirestore db;
-    private Button logOut, profile, teacherList;
-
     FirebaseAuth auth = FirebaseAuth.getInstance();
+    private Button logout, profile, studentlist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appstart);
-        String Username = getIntent().getStringExtra("user_uid");
-        TextView username = findViewById(R.id.Textname);
-        username.setText(Username);
-        logOut = findViewById(R.id.LogOut);
+        setContentView(R.layout.activity_app_start_teacher);
+        String username = getIntent().getStringExtra("user_uid");
+        TextView Username = findViewById(R.id.Textname);
+        Username.setText(username);
+        logout = findViewById(R.id.LogOut);
 
-        logOut.setOnClickListener(new View.OnClickListener() {
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 auth.signOut();
-                Intent intent = new Intent(appstart.this, MainActivity.class);
+                Intent intent = new Intent(appStartTeacher.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-
-
-        }
-
     }
-
+}
