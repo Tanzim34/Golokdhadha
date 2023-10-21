cc1069c943df13d277acc0f175dafd4ce58c4e84
Tanzim34 <tanzimshafin411@gmail.com>
2023-10-22 00:12:16 +0600

    Task management done

---
+++ app/src/main/java/com/example/myapplication/studteachactivity/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.studteachactivity;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
--- app/src/main/java/com/example/myapplication/studteachactivity/stdtask.java
+++ app/src/main/java/com/example/myapplication/studteachactivity/stdtask.java
@@ -1,16 +1,115 @@
 package com.example.myapplication.studteachactivity;
 
+import static com.example.myapplication.studteachactivity.AddNewTask.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.studentTeacher;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class stdtask extends AppCompatActivity {
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.myapplication.studteachactivity.ToDoAdapter;
+import com.example.myapplication.studteachactivity.ToDoModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class stdtask extends AppCompatActivity implements OnDialogCloseListner{
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton mFab;
+    private FirebaseFirestore firestore;
+    private ToDoAdapter adapter;
+    private List<ToDoModel> mList;
+    private Query query;
+    private ListenerRegistration listenerRegistration;
+
+    public String student_id, teacher_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stdtask);
+
+        student_id = getIntent().getStringExtra("student_id");
+        teacher_id = getIntent().getStringExtra("teacher_id");
+        recyclerView = findViewById(R.id.recyclerView);
+        mFab = findViewById(R.id.addButton);
+        firestore = FirebaseFirestore.getInstance();
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(stdtask.this));
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager() , TAG);
+            }
+        });
+
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(stdtask.this , mList);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new TouchHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+        showData();
+        recyclerView.setAdapter(adapter);
+    }
+    private void showData(){
+        DocumentReference studentRef = firestore.collection("Student").document("student_id"); // Use doc() to get a document
+        CollectionReference teacherCollectionRef = studentRef.collection("TeacherID"); // Access the subcollection
+        DocumentReference finalRef = teacherCollectionRef.document("teacher_id"); // Use doc() to get a document within the subcollection
+        query = finalRef.collection("task").orderBy("time" , Query.Direction.DESCENDING);
+
+        listenerRegistration = query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    // Handle the error
+                    Log.e(TAG, "Error getting documents: ", error);
+                    return;
+                }
+                else {
+                    for (DocumentChange documentChange : value.getDocumentChanges()) {
+                        if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                            String id = documentChange.getDocument().getId();
+                            ToDoModel toDoModel = documentChange.getDocument().toObject(ToDoModel.class).withId(id);
+                            mList.add(toDoModel);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    listenerRegistration.remove();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList.clear();
+        showData();
+        adapter.notifyDataSetChanged();
     }
 }
