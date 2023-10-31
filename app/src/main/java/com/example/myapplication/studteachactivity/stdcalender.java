3f285a6646666e636760f55e84e5d84f16194c49
Tanzim34 <tanzimshafin411@gmail.com>
2023-10-31 16:14:14 +0600

    Calender

--- app/src/main/java/com/example/myapplication/studteachactivity/stdcalender.java
+++ app/src/main/java/com/example/myapplication/studteachactivity/stdcalender.java
@@ -8,22 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
-import com.example.myapplication.sendRequest;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -33,8 +27,8 @@ public class stdcalender extends AppCompatActivity {
 
     private CalendarView calendarView;
     private EditText editText;
+    Button button;
     private String stringDateSelected;
-    private DatabaseReference databaseReference;
     FirebaseFirestore db;
     CollectionReference collectionReference;
 
@@ -50,62 +44,47 @@ public class stdcalender extends AppCompatActivity {
         std_id = getIntent().getStringExtra("student_id");
         teach_id = getIntent().getStringExtra("teacher_id");
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                stringDateSelected = i + Integer.toString(i1+1) + i2;
-                calendarClicked();
-            }
-        });
-        databaseReference = FirebaseDatabase.getInstance().getReference("Calender");
-        if(databaseReference==null)  Toast.makeText(stdcalender.this, "Error in database", Toast.LENGTH_SHORT).show();
-        databaseReference = databaseReference.child("stdID_" + std_id + "teachID_" + teach_id);
         db = FirebaseFirestore.getInstance();
         collectionReference = db.collection("Student").document(std_id).collection("TeacherID").document(teach_id).collection("Calender");
 
-    }
+        button = findViewById(R.id.button);
 
-    private void calendarClicked(){
-
-        DocumentReference documentReference = collectionReference.document(stringDateSelected);
-
-
-        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                Map<String, Object> request = new HashMap<>();
-                request.put("Date", stringDateSelected);
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                // Format the selected date to match your database structure
+                stringDateSelected = String.format("%04d-%02d-%02d", i, i1 + 1, i2);
+            }
+        });
 
-                collectionReference
-                        .add(request)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a reference to the selected date in the collection
+                DocumentReference documentReference = collectionReference.document(stringDateSelected);
+
+                // Create a data map with the event details
+                Map<String, Object> eventData = new HashMap<>();
+                eventData.put("Date", stringDateSelected);
+                eventData.put("EventDescription", editText.getText().toString());
+
+                // Add the event to Firestore
+                documentReference.set(eventData)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                                // Add your success message here
-                                Toast.makeText(stdcalender.this, "Request sent successfully", Toast.LENGTH_SHORT).show();
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot added for date: " + stringDateSelected);
+                                Toast.makeText(stdcalender.this, "Event added successfully", Toast.LENGTH_SHORT).show();
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Log.w(TAG, "Error adding document", e);
-                                // Add your failure message here
-                                Toast.makeText(stdcalender.this, "Failed to send request", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(stdcalender.this, "Failed to add event", Toast.LENGTH_SHORT).show();
                             }
                         });
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(stdcalender.this, "Failed", Toast.LENGTH_SHORT).show();
-
-            }
         });
-
-    }
-
-    public void buttonSaveEvent(View view){
-        databaseReference.child(stringDateSelected).setValue(editText.getText().toString());
     }
 }
